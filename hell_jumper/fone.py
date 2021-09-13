3d947faa33376c2988b9a4c4e7fed81bb71fb5ab
Gleb-is-404 <89805208+Gleb-is-404@users.noreply.github.com>
2021-09-13 16:55:19 +0300

    Update fone.py

--- hell_jumper/fone.py
+++ hell_jumper/fone.py
@@ -10,5 +10,7 @@ class fone_hell:
         self.image = pygame.image.load(('arts/Hell.png'))
         self.image = pygame.transform.scale(self.image, (1400, 800))
         self.rect = self.image.get_rect(center=(self.settings.width / 2, 400))
+    def update(self):
+        self.screen.blit(self.image, self.rect)
     def blitme(self):
         self.screen.blit(self.image, self.rect)
