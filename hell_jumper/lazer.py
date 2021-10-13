7e795e9b03d99dd2cf83c8d8abc7b6fff0f925a4
Gleb-is-404 <89805208+Gleb-is-404@users.noreply.github.com>
2021-10-13 19:56:30 +0300

    Update lazer.py

--- hell_jumper/lazer.py
+++ hell_jumper/lazer.py
@@ -1,46 +1,28 @@
 import pygame
-import time
 from random import randint
-from setting1 import Settings
 class lazer:
-    """A class to manage the ship."""
     def __init__(self, ai_game):
-        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.screen_rect = ai_game.screen.get_rect()
-        # Load the ship image and get its rect.
-        self.image = pygame.image.load(('arts/lazer_vermilion.png'))
-        self.image = pygame.transform.scale(self.image, (1400, 80))
-        self.image.set_colorkey(0, 0)
+        self.imwork('arts/lazer_vermilion.png')
         self.rect = self.image.get_rect(center=(self.settings.width / 2, self.settings.height / 2))
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
-        settings = Settings()
-        # Start each new ship at the bottom center of the screen.
         self.z = 0
         self.act = False
+    def imwork(self, name):
+        self.image = pygame.image.load((name))
+        self.image = pygame.transform.scale(self.image, (1400, 80))
+        self.image.set_colorkey(0, 0)
     def update(self):
         if self.z == 100:
-            self.image = pygame.image.load(('arts/lazer_vermilion_prepared.png'))
-            self.image = pygame.transform.scale(self.image, (1400, 80))
-            self.image.set_colorkey(0, 0)
+            self.imwork('arts/lazer_vermilion_prepared.png')
             self.act = False
             self.rect.y = randint(0, 700) 
         if self.z == 350:
-            self.image = pygame.image.load(('arts/lazer_vermilion.png'))
-            self.image = pygame.transform.scale(self.image, (1400, 80))
-            self.image.set_colorkey(0, 0)
+            self.imwork('arts/lazer_vermilion.png')
             self.act = True
         if self.z == 400:
                self.z = 0
         self.z += 1
-        self.screen.blit(self.image, self.rect)
-    def dethe(self):
-        self.Agent_R_M_D.x = self.settings.width / 2
-        self.Agent_R_M_D.y = -(self.settings.height) + 800
-        self.settings.grav = 0
-        self.Agent_R_M_D.jamp = False
-    def blitme(self):
-        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
