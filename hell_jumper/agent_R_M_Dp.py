299ed1c6867601e0063097ee7fced985f9f0e047
Gleb-is-404 <89805208+Gleb-is-404@users.noreply.github.com>
2021-10-13 19:56:04 +0300

    Update agent_R_M_Dp.py

--- hell_jumper/agent_R_M_Dp.py
+++ hell_jumper/agent_R_M_Dp.py
@@ -3,12 +3,10 @@ import time
 class agent_R_M_D:
     """A class to manage the ship."""
     def __init__(self, ai_game):
-        """Initialize the ship and set its starting position."""
-
+        self.grav = 1
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        # Load the ship image and get its rect.
         self.image = pygame.image.load(('arts/agent.R.M.D.png'))
         self.image = pygame.transform.scale(self.image, (70, 140))
         self.image.set_colorkey(0, 0)
@@ -17,7 +15,6 @@ class agent_R_M_D:
         self.y = float(self.rect.y)
         self.moving_right = False
         self.moving_left = False
-        self.gravety = True
         self.jamp = False
         self.speeds = False
     def update(self):
@@ -26,21 +23,13 @@ class agent_R_M_D:
         if self.moving_right and self.rect.right <= self.screen_rect.right:
             self.x += self.settings.speed
         if self.jamp and self.rect.top >= self.screen_rect.top:
-            self.settings.grav = -2
+            self.grav = -2
         if self.rect.top == self.screen_rect.top:
             self.jamp = False
-            self.settings.grav = 1
+            self.grav = 1
         self.rect.x = self.x
         self.rect.y = self.y
         if self.speeds:
-            self.settings.grav += 0.02
-            self.settings.grav = round(self.settings.grav, 2)
-        if self.gravety == True:
-            self.y += self.settings.grav
-            if self.settings.grav < 4:
-                self.settings.grav += 0.02
-                self.settings.grav = round(self.settings.grav, 2)
-        self.screen.blit(self.image, self.rect)
-    def blitme(self):
-        """Draw the ship at its current location."""
+            self.grav += 0.02
+            self.grav = round(self.grav, 2)
         self.screen.blit(self.image, self.rect)
